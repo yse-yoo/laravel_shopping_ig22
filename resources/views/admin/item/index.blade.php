3b12c529c02ed702626aac50ed7d26db1752b7b6
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-01-26 10:31:24 +0900

    商品一覧処理追加

--- resources/views/admin/item/index.blade.php
+++ resources/views/admin/item/index.blade.php
@@ -1,3 +1,21 @@
 <h2>商品一覧</h2>
-
 <a href="{{ route('admin.item.create') }}">新規追加(New Item)</a>
+
+<table class="table">
+    <tr>
+        <th></th>
+        <th>{{ __('Item Name') }}</th>
+        <th>{{ __('Item Code') }}</th>
+        <th>{{ __('Price') }}</th>
+    </tr>
+    @if($items)
+    @foreach($items as $item)
+    <tr>
+        <td></td>
+        <td>{{ $item->name }}</td>
+        <td>{{ $item->code }}</td>
+        <td>{{ $item->price }}</td>
+    </tr>
+    @endforeach
+    @endif
+</table>
